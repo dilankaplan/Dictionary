ec50e743068ba8fde206d45817598a1cd7f1b6bc
dilankaplan <45143995+dilankaplan@users.noreply.github.com>
2021-07-17 20:49:26 +0300

    Proje dosyası ekle.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> yemekler = new MyList<string>();
+            yemekler.Add("Hamburger");
+            yemekler.Add("Pizza");
+            yemekler.Add("Makarna");
+            Console.WriteLine(yemekler.Count);
+
+            MyList<string> saglikliYemekler = new MyList<string>();
+            saglikliYemekler.Add("Ispanak");
+            saglikliYemekler.Add("Kabak");
+            saglikliYemekler.Add("Patlican");
+            saglikliYemekler.Add("Yogurt");
+
+            Console.WriteLine(saglikliYemekler.Count);
+
+        }
+    }
+}
+
+class MyList<T>
+{
+    T[] yemekArray;
+    T[] tempArray;
+    public MyList()
+    {
+        yemekArray = new T[0];
+    }
+    public void Add(T item)
+    {
+        tempArray = yemekArray;
+        yemekArray = new T[yemekArray.Length + 1];
+        for (int i = 0; i < tempArray.Length-1; i++)
+        {
+            yemekArray[i] = tempArray[i];
+        }
+
+    }
+
+    public int Count
+    {
+        get { return yemekArray.Length; }
+    }
+}
